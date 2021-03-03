f475684edf02d2760a342a5a2739026657f85d11
dungnguyensarasota <dung.nguyen.sarasota@gmail.com>
2021-03-03 18:11:37 -0500

    Created portfolio.py file and made bokeh_pplot pep8 compliance

--- Bokeh_Plot.py
+++ Bokeh_Plot.py
@@ -5,11 +5,13 @@ from bokeh.io import output_notebook, show
 from bokeh.plotting import figure
 # output_notebook()
 import bokeh.sampledata
+
 bokeh.sampledata.download()
 
-from bokeh.sampledata.autompg import autompg as df # run df.head() to inspect
-# Take first 10 rows 
-df1=df.iloc[0:11,]
+from bokeh.sampledata.autompg import autompg as df  # run df.head() to inspect
+
+# Take first 10 rows
+df1 = df.iloc[0:11, ]
 # # print(df.head())
 p = figure(plot_width=400, plot_height=400)
 #
@@ -23,15 +25,14 @@ from bokeh.sampledata.stocks import AAPL
 # AAPL.keys()
 # dict_keys(['date', 'open', 'high', 'low', 'close', 'volume', 'adj_close'])
 # print(AAPL)
-dates = np.array(AAPL['date'], dtype=np.datetime64) # convert date strings to real datetimes
-close = np.array(AAPL['close'],dtype=np.float_)
+dates = np.array(AAPL['date'], dtype=np.datetime64)  # convert date strings to real datetimes
+close = np.array(AAPL['close'], dtype=np.float_)
 p = figure(x_axis_type="datetime", title="AAPL Close Price", plot_height=350, plot_width=800)
-p.xgrid.grid_line_color=None
-p.ygrid.grid_line_alpha=0.5
+p.xgrid.grid_line_color = None
+p.ygrid.grid_line_alpha = 0.5
 p.xaxis.axis_label = 'Time'
 p.yaxis.axis_label = 'Close Price'
 
 p.line(dates, close)
 
 show(p)
-
